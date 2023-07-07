8a8badd12d97ed31da8575b8084d17d389d24298
V-Hugo-C <victorwise@hotmail.com>
2023-07-07 17:09:08 -0300

    feat: Adicionado os metodos para cadastrar um valor de estacionamento e o de cadastrar um evento quando tiver

--- Entrega2/Estacionamento.java
+++ Entrega2/Estacionamento.java
@@ -4,12 +4,19 @@ public class Estacionamento {
     private int capacidade;
     private String local;
     private int vagasOcupadas;
+    private Valores valor
 
     public Estacionamento(int capacidade, String local) {
         this.capacidade = capacidade;
         this.local = local;
         this.vagasOcupadas = 0;
     }
+    public void cadastrarValoresEstacionamento(float valorFracao, float valorDiaria, float valorNoturno, float valorMensal, float valorEvento){
+        valor = new Valores(valorFracao, valorDiaria, valorNoturno, valorMensal, valorEvento);
+    }
+    public void cadastrarEventos(String nome, Data dataInicio, Data dataFinal, Horario inicio, Horario fim){
+        evento = new Evento(nome, dataInicio, dataFinal, inicio, fim);
+    } 
 
     public void CalculoContratante(int vagasContratadas) {
         if (vagasContratadas > capacidade) {
