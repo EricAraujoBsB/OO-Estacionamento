89126d6e159c8906a78ca4a73f7ffbc87159b362
indalves <138887637+indalves@users.noreply.github.com>
2023-07-07 15:35:04 -0300

    Create Evento.java

---
+++ Entrega2/Evento.java
@@ -0,0 +1,70 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Eventos {
+    private String nomeEvento;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private LocalTime horaInicio;
+    private LocalTime horaFim;
+    private int capacidadeMaxima;
+
+    private Eventos() {}
+
+    public Eventos(String nomeEvento, LocalDate dataInicio, LocalDate dataFim, LocalTime horaInicio, LocalTime horaFim, int capacidadeMaxima) {
+        this.nomeEvento = nomeEvento;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    public String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public void setNomeEvento(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(LocalTime horaFim) {
+        this.horaFim = horaFim;
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public void setCapacidadeMaxima(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+}
