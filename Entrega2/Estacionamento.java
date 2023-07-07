69bec7f99f640052680e82f5b7898831d787f93b
Loliveirinha <101184699+LORliveira@users.noreply.github.com>
2023-07-07 18:14:02 -0300

    Update Estacionamento.java

--- Entrega2/Estacionamento.java
+++ Entrega2/Estacionamento.java
@@ -1,10 +1,13 @@
+
+
+
 package OOP;
 
 public class Estacionamento {
     private int capacidade;
     private String local;
     private int vagasOcupadas;
-    private Valores valor
+    private Valores valor;
 
     public Estacionamento(int capacidade, String local) {
         this.capacidade = capacidade;
@@ -30,5 +33,31 @@ public class Estacionamento {
     public void verificaLotacao() {
         int vagasDisponiveis = capacidade - vagasOcupadas;
         System.out.println("Vagas disponíveis: " + vagasDisponiveis);
+    }
+
+    public void CadastrarAcesso(String placa, Horario entrada, Horario saida, Data dataEntrada, Data dataSaida) {
+        int Hora = 18; 
+        int Minuto = 0;
+    
+        if ((entrada.getHora() > Hora) || (entrada.getHora() == Hora && entrada.getMinuto() > Minuto)
+        || (saida.getHora() > Hora) || (saida.getHora() == Hora && saida.getMinuto() > Minuto)) {
+            System.out.println("Nao e possivel cadastrar o acesso.");
+        } else if (dataEntrada.getDia() > 30 || dataSaida.getDia() > 30) {
+            System.out.println("Nao e possivel cadastrar o acesso.");
+        } else {
+            System.out.println("Acesso cadastrado com sucesso.");
+        }
+    }
+    public String getCapacidade(){
+        return capacidade;
+    }
+    public String getLocal(){
+        return local;
+    }
+    public String getVagasOcupadas(){
+        return vagasOcupadas;
+    }
+    public String getValor(){
+        return valor;
     }
 }
