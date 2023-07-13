ffe9b292ce5a54abded54238151200330e8a6f0c
Emivalto Junior <emivalto14@gmail.com>
2023-07-13 11:08:54 -0300

    Git add .

--- Entrega2/AcessoDiaria.java
+++ Entrega2/AcessoDiaria.java
@@ -9,8 +9,8 @@ public class AcessoDiaria extends Acesso {
 
   @Override
   public float calcularValor(Valores valor, Horario entrada, Horario saida) {
-    if((getEntrada().getHora() >= fechar.getHora() && getEntrada().getMinuto() >= fechar.getMinuto())
-        && (getSaida().getHora() <= abrir.getHora() && getSaida().getMinuto() <= abrir.getMinuto())) {
+    if((getEntrada.getHora() >= fechar.getHora() && getEntrada.getMinuto() >= fechar.getMinuto())
+        && (getSaida.getHora() <= abrir.getHora() && getSaida.getMinuto() <= abrir.getMinuto())) {
       return valor.getValorNoturno();
     }else {
       return valor.getValorDiaria();
